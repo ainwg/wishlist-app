f8ff8981f609a23911da7762ea9f74ef4ba72986
ainwg <87463461+ainwg@users.noreply.github.com>
2025-12-01 14:19:15 +0800

    Update outline text field

---
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -0,0 +1,71 @@
+package com.example.wishlistapp
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun AddEditDetailView(
+    id: Long,
+    viewModel: WishViewModel,
+    navController: NavController
+) {
+    Scaffold(
+        topBar = { AppBarView(title =
+        if(id != 0L) "Update Wish" else "Add Wish") }
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
+                .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {Text(text = label, color = Color.Black)},
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            // using predefined colors
+            textColor = Color.Black
+        )
+    )
+}
+
+@Preview
+@Composable
+fun WishTestFieldPrev() {
+        WishTextField(label = "text", value = "text", onValueChange = {})
+}
+
+
